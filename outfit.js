c1b1c853acb5273174ed3a2d534f0167a73c7888
sabirof <maharram.sabir@gmail.com>
2023-04-25 21:54:04 +0200

    new

--- outfit.js
+++ outfit.js
@@ -1,40 +1,53 @@
-// Function to get weather data from API
-function getWeatherData(city) {
-  const apiKey = "fdfc26ec7e877ffa8987ba56f2782ae4";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fdfc26ec7e877ffa8987ba56f2782ae4&units=metric`;
+const apiKey = "fdfc26ec7e877ffa8987ba56f2782ae4";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fdfc26ec7e877ffa8987ba56f2782ae4&units=metric`;
 
-  // Function to get outfit suggestion based on weather data
-function getOutfitSuggestion() {
-  const city = document.querySelector('#city').value;
-  const apiKey = 'your_api_key_here';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function getWeatherData(city) {
+  function getOutfitSuggestion(data) {
+    const temperature = Math.round(data.main.temp - 273.15);
+    const weatherDescription = data.weather[0].description;
+
+    let outfit = "";
+
+    if (temperature > 25 && weatherDescription.includes("clear")) {
+      outfit = "Wear a t-shirt, shorts, and sandals.";
+    } else if (temperature > 20 && weatherDescription.includes("clear")) {
+      outfit = "Wear a t-shirt, shorts, and sneakers.";
+    } else if (
+      temperature > 15 &&
+      (weatherDescription.includes("clear") ||
+        weatherDescription.includes("clouds"))
+    ) {
+      outfit = "Wear a light jacket, t-shirt, jeans, and sneakers.";
+    } else if (
+      temperature > 10 &&
+      (weatherDescription.includes("clouds") ||
+        weatherDescription.includes("rain"))
+    ) {
+      outfit = "Wear a raincoat, t-shirt, jeans, and sneakers.";
+    } else if (temperature > 0 && weatherDescription.includes("snow")) {
+      outfit = "Wear a heavy jacket, sweater, pants, and boots.";
+    } else {
+      outfit = "Sorry, we couldn't find an outfit for these conditions.";
+    }
+
+    const outfitDiv = document.getElementById("#outfit");
+    outfitDiv.innerHTML = outfit;
+  }
 
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const temperature = Math.round(data.main.temp - 273.15);
-      const weatherDescription = data.weather[0].description;
-      
-      let outfit = '';
-  
-      if (temperature > 25 && weatherDescription.includes('clear')) {
-        outfit = 'Wear a t-shirt, shorts, and sandals.';
-      } else if (temperature > 20 && weatherDescription.includes('clear')) {
-        outfit = 'Wear a t-shirt, shorts, and sneakers.';
-      } else if (temperature > 15 && (weatherDescription.includes('clear') || weatherDescription.includes('clouds'))) {
-        outfit = 'Wear a light jacket, t-shirt, jeans, and sneakers.';
-      } else if (temperature > 10 && (weatherDescription.includes('clouds') || weatherDescription.includes('rain'))) {
-        outfit = 'Wear a raincoat, t-shirt, jeans, and sneakers.';
-      } else if (temperature > 0 && weatherDescription.includes('snow')) {
-        outfit = 'Wear a heavy jacket, sweater, pants, and boots.';
-      } else {
-        outfit = 'Sorry, we couldn\'t find an outfit for these conditions.';
-      }
-      
-      const outfitDiv = document.querySelector('#outfit');
-      outfitDiv.innerHTML = outfit;
+    .then((response) => response.json())
+    .then((data) => {
+      getOutfitSuggestion(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 }
+
+//1. add change/input event to input element (lookup the syntax)
+//inside the callback of that event you should be able to CONSOLE.LONG whatever you type
+//Either you create an event that works when pushing ENTER, or you add a click event to the button
+//either on Enter, or on click, we call the Fetch function* witht the value of the input
+
+//*create a fetch function that recieves the city text, and insert it in the url
+//when you get the result, you call the function that checks the wheather conditions, and based on that, generates the HTML card with the suggestions
--- weather.js
+++ weather.js
@@ -1,71 +1,104 @@
+// Getting the current weather data from API
+const apiKey = "fdfc26ec7e877ffa8987ba56f2782ae4";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const iconApiUrl = "https://openweathermap.org/img/wn/10d@2x.png";
+
 const citybtn = document.getElementById("citybtn");
 citybtn.addEventListener("click", getWeather);
 
-const iconCode2 = async (data) => {
-  console.log(data.weather[0].icon);
-};
-// template literals https://openweathermap.org/img/wn/04d@2x.png
 async function getWeather() {
   const city = document.getElementById("city").value;
   const apiKey = "fdfc26ec7e877ffa8987ba56f2782ae4";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fdfc26ec7e877ffa8987ba56f2782ae4&units=metric`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const iconApiUrl = "https://openweathermap.org/img/wn/10d@2x.png";
 
-  await fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const weatherInfo = document.getElementById("weather-card");
-      weatherInfo.innerHTML = `
-        <h2>${data.name} Weather</h2>
-        <p>Temperature: ${data.main.temp} &deg;C</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        <p>Description: ${data.weather[0].description}</p>
-      `;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-      const iconCode = data.weather[0].icon;
-      const iconElement = document.createElement("img");
-      iconElement.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
-      console.log("iconElement.src :>> ", iconElement.src);
-      const iconContainer = document.getElementById("icon-container");
-      iconContainer.innerHTML = "";
-      iconContainer.appendChild(iconElement);
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-    });
+    const weatherInfo = document.getElementById("weather-card");
+    weatherInfo.innerHTML = `
+      <h2>${data.name} Weather</h2>
+      <p>Temperature: ${data.main.temp} &deg;C</p>
+      <p>Humidity: ${data.main.humidity}%</p>
+      <p>Description: ${data.weather[0].description}</p>
+    `;
+
+    const iconCode = data.weather[0].icon;
+    const iconElement = document.createElement("img");
+    iconElement.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
+    const iconContainer = document.getElementById("icon-container");
+    iconContainer.innerHTML = "";
+    iconContainer.appendChild(iconElement);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
 }
 
-// Getting the forecast data from  API
-
-const searchButton = document.querySelector("#search-button");
-const searchInput = document.querySelector("#search-city");
+// Getting the weekly forecast data from  API
+//! NO GLOBAL VARIABLES
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-city");
+const weeklyWeather = document.getElementById("weekly-weather");
+const icons = document.getElementById("weather-icon");
 
 searchButton.addEventListener("click", () => {
   const city = searchInput.value;
   fetchWeatherData(city);
 });
+
 async function fetchWeatherData(city) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=fdfc26ec7e877ffa8987ba56f2782ae4&units=metric`
   );
+
   const data = await response.json();
+  console.log(data);
   displayWeatherData(data);
 }
+
 function displayWeatherData(data) {
+  console.log("data :>> ", data);
+  console.log("data :>> ", data.list);
   const forecast = data.list;
-  const weeklyWeather = document.querySelector("#weekly-weather");
 
   for (let i = 0; i < forecast.length; i += 8) {
     const day = new Date(forecast[i].dt_txt).toLocaleDateString("en-US", {
       weekday: "long",
     });
     const weather = forecast[i].weather[0].description;
+
     const highTemp = forecast[i].main.temp_max.toFixed(0);
     const lowTemp = forecast[i].main.temp_min.toFixed(0);
-
+    const iconKey = forecast[i].weather[0].icon;
+    //create a variable that contains an URL with a dynamic value, being the iconkey the variable element
+    const iconURL = "https://openweathermap.org/img/wn/10d@2x.png";
+    const iconImg = document.getElementById("icon-img");
+    iconImg.setAttribute("src", iconURL);
     const row = weeklyWeather.rows[i / 8];
     row.cells[0].textContent = day;
     row.cells[1].textContent = weather;
-    row.cells[2].querySelector(".high-temp").textContent = `${highTemp}°C`;
-    row.cells[3].querySelector(".low-temp").textContent = `${lowTemp}°C`;
+    row.cells[2].getElementsByClassName(
+      "high-temp"
+    )[0].textContent = `${highTemp}°C`;
+    row.cells[3].getElementsByClassName(
+      "low-temp"
+    )[0].textContent = `${lowTemp}°C`;
   }
 }
+
+// function to get my data icons
+// fetch(ApiUrl)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const conditionCode = data.conditionCode;
+//     fetch(iconApiUrl + conditionCode)
+//       .then((response) => {})
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   })
+//   .catch((error) => {
+//     //
+//     console.log(error);
+//   });
